a8f052b4bde53a59d4b7abddc7125103402ffe2b
rambabumuddasani <rambabu0006@gmail.com>
2022-05-29 16:19:00 +0530

    Test impl

--- src/main/java/com/example/twitter/TwitterApplication.java
+++ src/main/java/com/example/twitter/TwitterApplication.java
@@ -7,7 +7,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TwitterApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hellow World");
+		System.out.println("Hellow World2");
 		SpringApplication.run(TwitterApplication.class, args);
 	}
+
+	public int add(int a,int b){
+
+		
+		return a+b;
+	}
+
+	public int sub(int a,int b){
+		return a-b;
+	}
+
+	public int mul(int a,int b){
+		return a*b;
+	}
+
+	public int div(int a,int b){
+		return a/b;
+	}
+
 
 }
